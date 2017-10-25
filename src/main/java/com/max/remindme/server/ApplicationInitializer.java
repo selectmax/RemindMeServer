36cc02081aa8e73e53500aa5012fb0fde1fa3fc8
Макс <maxim1801@mail.ru>
2017-10-25 21:32:02 +0300

    Prepeared base project

---
+++ src/main/java/com/max/remindme/server/ApplicationInitializer.java
@@ -0,0 +1,27 @@
+package com.max.remindme.server;
+
+import com.max.remindme.server.config.WebConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Created by max on 25.10.2017.
+ */
+public class ApplicationInitializer implements WebApplicationInitializer{
+
+    private final static String DISPATCHER = "dispatcher";
+
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebConfig.class);
+        servletContext.addListener(new ContextLoaderListener(ctx));
+        ServletRegistration.Dynamic servlet = servletContext.addServlet(DISPATCHER, new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
---
+++ src/main/java/com/max/remindme/server/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.max.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.max.remindme.server")
+public class WebConfig  extends WebMvcConfigurerAdapter{
+}
